057bcffa94601353b997d6ae842f628c3dc8b524
hvhai <hunterx6323@gmail.com>
2023-07-21 08:43:04 +0700

    R-1.2

---
+++ src/main/java/org/codehunter/chap1/R12.java
@@ -0,0 +1,37 @@
+package org.codehunter.chap1;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Suppose that we create an array A of GameEntry objects,
+ * which has an integer scores field, and we clone A and store the result in an array B.
+ * If we then immediately set A[4].score equal to 550,
+ * what is the score value of the GameEntry object referenced by B[4]?
+ */
+public class R12 {
+    public static final Logger log = LogManager.getLogger(R12.class);
+    public static void main(String[] args) {
+        GameEntry[] a = {
+                new GameEntry(0),
+                new GameEntry(1),
+                new GameEntry(2),
+                new GameEntry(3),
+                new GameEntry(4),
+                new GameEntry(5),
+        };
+        GameEntry[] b = a.clone();
+        a[4].score = 550;
+        log.info(b[4].score);
+
+
+    }
+
+    static class GameEntry {
+        int score;
+
+        public GameEntry(int score) {
+            this.score = score;
+        }
+    }
+}
