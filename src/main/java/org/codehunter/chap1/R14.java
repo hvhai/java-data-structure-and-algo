7357fac8f213bde55aab6a8d0640b54a66c1f941
hvhai <hunterx6323@gmail.com>
2023-07-24 09:09:16 +0700

    R-1.4: add even check by binary

--- src/main/java/org/codehunter/chap1/R14.java
+++ src/main/java/org/codehunter/chap1/R14.java
@@ -6,15 +6,18 @@ import org.apache.logging.log4j.Logger;
 /**
  * Write a short Java method, isEven, that takes an int i and returns true if and only if i is even.
  * Your method cannot use the multiplication, modulus, or division operators, however.
- *
  */
 public class R14 {
     public static final Logger log = LogManager.getLogger(R14.class);
+
     public static void main(String[] args) {
-        log.info(Integer.MAX_VALUE);
-        log.info("is even {} ", isEven(Integer.MAX_VALUE));
+        int testValue = 4894;
+        log.info("Test value: {}", testValue);
+        log.info("isEven: {} ", isEven(testValue));
+        log.info("isEvenByBinaryCheck: {} ", isEvenByBinaryCheck(testValue));
     }
 
+    // this can throw StackoverflowException
     static boolean isEvenRecursive(int i) {
         if (i < 0) {
             log.error("negative");
@@ -22,15 +25,21 @@ public class R14 {
         }
         if (i == 0) return true;
         if (i == 1) return false;
-        return isEvenRecursive(i-2);
+        return isEvenRecursive(i - 2);
     }
 
     static boolean isEven(int i) {
         while (i > 0) {
-            i = i-2;
+            i = i - 2;
             if (i == 0) return true;
             if (i == 1) return false;
         }
         return false;
+    }
+
+    static boolean isEvenByBinaryCheck(int i) {
+        String binaryString = Integer.toBinaryString(i);
+        log.info("binary string of {} is : {} ", i, binaryString);
+        return binaryString.charAt(binaryString.length() - 1) == '0';
     }
 }
