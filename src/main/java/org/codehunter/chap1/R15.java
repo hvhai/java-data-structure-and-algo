02a9b80e7a20e7eeb43123adeeaceb380eb1cb57
hvhai <hunterx6323@gmail.com>
2023-07-24 15:41:55 +0700

    R-1.5: change i type to long

--- src/main/java/org/codehunter/chap1/R15.java
+++ src/main/java/org/codehunter/chap1/R15.java
@@ -3,30 +3,41 @@ package org.codehunter.chap1;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 /**
  * Write a short Java method that takes an integer n and returns the sum of all positive integers less than or equal to n.
  */
 public class R15 {
     public static final Logger log = LogManager.getLogger(R15.class);
+
     public static void main(String[] args) {
-        int value = 9;
-        log.info("Sum of {} is: {}", value, sum(value));
-        log.info("SumBySteam of {} is: {}", value, sumByStream(value));
+        int value = Integer.MAX_VALUE;
+        long currentTime = System.currentTimeMillis();
+        log.info("Sum of {} is: {} | Calculated in {}", value, sum(value), System.currentTimeMillis() - currentTime);
+
+        currentTime = System.currentTimeMillis();
+        log.info("SumBySteam of {} is: {} | Calculated in {}", value, sumByStream(value), System.currentTimeMillis() - currentTime);
+
+        currentTime = System.currentTimeMillis();
+        log.info("SumByParallelSteam of {} is: {} | Calculated in {}", value, sumByParallelStream(value), System.currentTimeMillis() - currentTime);
     }
 
-    static int sum(int n) {
-        int sum = 0;
-        int i = 0;
+    static long sum(int n) {
+        long sum = 0;
+        long i = 0;
         while (i <= n) {
-            sum +=i;
+            sum += i;
             i++;
         }
         return sum;
     }
 
-    static int sumByStream(int n) {
-        return IntStream.rangeClosed(0, n).sum();
+    static long sumByStream(int n) {
+        return LongStream.rangeClosed(0, n).sum();
+    }
+
+    static long sumByParallelStream(int n) {
+        return LongStream.rangeClosed(0, n).parallel().sum();
     }
 }
