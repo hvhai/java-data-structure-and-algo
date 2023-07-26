3e47df910db701ba979d3adb115f36473f5c4c54
hvhai <hunterx6323@gmail.com>
2023-07-26 08:10:47 +0700

    R-1.9 regex solution

--- src/main/java/org/codehunter/chap1/R19.java
+++ src/main/java/org/codehunter/chap1/R19.java
@@ -10,18 +10,36 @@ import org.apache.logging.log4j.Logger;
  */
 public class R19 {
     public static final Logger log = LogManager.getLogger(R19.class);
+
     public static void main(String[] args) {
-        log.info("A: {} ,z: {}", (int)'A', (int)'z');
+        log.info("A: {} ,z: {}", (int) 'A', (int) 'z');
         String s = "Let’s try, Mike!";
-        log.info("removeAllPunctuationWithStream of {} is\n{} ",s, removeAllPunctuationWithStream(s));
+        String removeAllPunctuationWithStream = removeAllPunctuationWithStream(s);
+        log.info("removeAllPunctuationWithStream of {} is\n{} ", s, removeAllPunctuationWithStream);
+        String removePunctuations = removePunctuations(s);
+        log.info("removePunctuations of {} is\n{} ", s, removePunctuations);
     }
 
     static String removeAllPunctuationWithStream(String s) {
+        if (s == null) return "";
         return s.chars()
-            .filter(c -> c > 64 && c < 123 || c == ' ')
-            .mapToObj(Character::toString)
-            .reduce(String::concat)
-            .orElse("");
+                .filter(c -> c > 64 && c < 123 || c == ' ')
+                .mapToObj(Character::toString)
+                .reduce(String::concat)
+                .orElse("");
     }
 
+    static String removePunctuations(String string) {
+        if (string == null) return "";
+        /*
+         * replace all method takes two arguments first one is regex pattern in which
+         * you mention pattern applicable to remove all punctions in the string and in
+         * the second argument we pass the which one we want to replace with regex
+         * pattern to remove all punctuations look like below.
+         */
+        string = string.replaceAll("\\p{P}", "");
+
+        return string;
+
+    }
 }
